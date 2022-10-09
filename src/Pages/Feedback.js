246517714585e7e09b13a413f6d9e72f5f9c35c4
Isa Costa <isa-hcosta@hotmail.com>
2022-10-09 20:21:06 -0300

    commit para rodar o evaluator novamente

--- src/Pages/Feedback.js
+++ src/Pages/Feedback.js
@@ -4,6 +4,7 @@ class Feedback extends React.Component {
   render() {
     return (
       <h1 data-testid="feedback-text">Feedback</h1>
+      //Alteração para rodar o gitHub novamente
     );
   }
 }
