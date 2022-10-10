69ab77e6ab1f42d8d58d24e479f35b3e12a0ac05
Isa Costa <isa-hcosta@hotmail.com>
2022-10-10 13:51:17 -0300

    requisito 15 e 16

--- src/Pages/Ranking.js
+++ src/Pages/Ranking.js
@@ -1,30 +1,11 @@
 import React from 'react';
 
 class Ranking extends React.Component {
-  handleClick = () => {
-    const { history } = this.props;
-    history.push('/');
-  };
-
   render() {
     return (
-      <>
-        <h1 data-testid="ranking-title">Ranking</h1>
-        <button
-          type="button"
-          data-testid="btn-go-home"
-          onClick={ this.handleClick }
-        >
-          Play Again
-        </button>
-      </>
+      <h1 data-testid="ranking-title">Ranking</h1>
     );
   }
 }
-
-Ranking.propTypes = {
-  history: PropTypes.shape({}),
-  push: PropTypes.func,
-}.isRequired;
 
 export default Ranking;
