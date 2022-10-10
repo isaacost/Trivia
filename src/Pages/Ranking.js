0cd354ce2c34022f476d7d6c090fda04403aebee
Isa Costa <isa-hcosta@hotmail.com>
2022-10-10 13:41:33 -0300

    requisito 15, 16 e 18

--- src/Pages/Ranking.js
+++ src/Pages/Ranking.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
 class Ranking extends React.Component {
+  handleClick = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
-      <h1>Ranking</h1>
+      <>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.handleClick }
+        >
+          Play Again
+        </button>
+      </>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({}),
+  push: PropTypes.func,
+}.isRequired;
 
 export default Ranking;
